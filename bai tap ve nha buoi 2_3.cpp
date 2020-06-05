91829cd646e47cba1e2d49e3a530b1c30f49e850
pham-ha <phamhanv78@gmail.com>
2020-06-05 14:03:38 +0700

    Create bai tap ve nha buoi 2_3.cpp

---
+++ bai tap ve nha buoi 2_3.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main(){
+	int a, b, c;
+	printf("nhap so a: ");
+	scanf("%d", &a)
+	printf("nhap so b: ");
+	scan("%d", &b)
+	printf("nhap so c: ");
+	scan("%d", &c);
+	if(a = 0){
+		if(b = 0){
+			printf("vo nghiem");
+		}else{
+			if(c = 0){
+				printf("vo so nghiem");
+			}else{
+				printf("X = -c/b");
+	}else{
+		printf("b - 4ac");
+		if(d < 0)
+			print("vo nghiem");
+		}else{
+			if(d=0)
+				printf(x1 = ...){
+			}else{
+				printf("x1 = -b+ /2a");
+				printf("x2 = -b- /2a");
+			}
+				}
+		}
+	}
+		}
+		}
+	}
+}
+
+
