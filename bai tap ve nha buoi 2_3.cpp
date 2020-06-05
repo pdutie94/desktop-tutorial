6411d7fa1b0decf61f08746101cb565f31e5bbc7
Pham Duc Tien <ptiencntt94@gmail.com>
2020-06-05 14:32:31 +0700

    The quadratic equation 2

--- bai tap ve nha buoi 2_3.cpp
+++ bai tap ve nha buoi 2_3.cpp
@@ -1,37 +1,37 @@
 #include <stdio.h>
+#include <math.h>
+
 int main(){
-	int a, b, c;
+	float a, b, c, delta;
 	printf("nhap so a: ");
-	scanf("%d", &a)
+	scanf("%f", &a);
 	printf("nhap so b: ");
-	scan("%d", &b)
+	scanf("%f", &b);
 	printf("nhap so c: ");
-	scan("%d", &c);
-	if(a = 0){
-		if(b = 0){
-			printf("vo nghiem");
-		}else{
-			if(c = 0){
-				printf("vo so nghiem");
-			}else{
-				printf("X = -c/b");
-	}else{
-		printf("b - 4ac");
-		if(d < 0)
-			print("vo nghiem");
-		}else{
-			if(d=0)
-				printf(x1 = ...){
-			}else{
-				printf("x1 = -b+ /2a");
-				printf("x2 = -b- /2a");
-			}
-				}
+	scanf("%f", &c);
+	
+	if (a == 0) {
+	    if (b == 0) {
+		printf("Phuong trinh vo nghiem");    
+	    } else {
+		if (c == 0) {
+		    printf("Phuong trinh co vo so nghiem");
+		} else {
+		    printf("Phuong trinh co 1 nghiem (x = -c/b): %.1f", (-c/b));
 		}
-	}
-		}
-		}
+	    }
+	} else {
+	    delta = (b*b - 4*a*c);
+	    if (delta < 0) {
+		printf("Phuong trinh vo nghiem");    
+	    }
+	    if (delta == 0) {
+		printf("Phuong trinh co nghiem kep (x1 = x1 = (-b/(2a)): %.1f", (-b/(2*a)));
+	    }
+	    if (delta > 0) {
+		printf("Phuong trinh co 2 nghiem:");    
+		printf("\nx1 = %.1f", (-b + sqrt(delta))/(2*a));    
+		printf("\nx2 = %.1f", (-b - sqrt(delta))/(2*a));    
+	    }
 	}
 }
-
-
